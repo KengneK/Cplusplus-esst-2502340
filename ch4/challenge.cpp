605c74dc74ab65dabf16e60343c4cdff8db1b8fd
KengneK <kkengne.kevin@yahoo.com>
2024-04-22 01:29:33 +0200

    challenge ch4 fertig!

--- ch4/challenge.cpp
+++ ch4/challenge.cpp
@@ -28,19 +28,40 @@ struct Customer {
 bool verifyStatus(const Customer &cust)
 {
 	// TODO: Alle Karten müssen gültig (valid) sein.
+    if(cust.mCreditcard.mState != CreditCardState::Valid) return false;
 	// TODO: Premium-Kunden dürften sich weiterhin verschulden, Standard-Kunden nicht
+    if(cust.mState == CustomerState::Premium) return true;
+
+    if(cust.mCreditcard.mBalance < 0) return false;  // Reihenfolgen von if-Anweisugen ist Wichtig. Ohne diese if-Anweisung muss in withdrawMoney der kommentierte Code betrachtet werden
+
+    return true;
 }
 
 void withdrawMoney(Customer &cust, const int amnt)
 {
 	// TODO: Prüfen ob Kunde überhaupt Geld abheben darf; Fehler melden, falls nicht.
+    if( verifyStatus(cust) == false){
+        std::cout << "withdraw Money impossible!!" << std::endl;
+        return;
+    }
+
+    /*if(cust.mState != CustomerState::Premium){
+        if(cust.mCreditcard.mBalance < 0){
+            std::cout << "withdraw Money impossible because not enought Money!!" << std::endl;
+            return;
+        }
+
+    }*/
+    
 	
-	// TODO: Dann Geld abheben    
+	// TODO: Dann Geld abheben  
+    cust.mCreditcard.mBalance -= amnt;  
 }
 
 void showBalance(const CreditCard &card)
 {
 	// TODO: Anzeigen wieviel Geld noch auf der Kreditkarte ist
+    std::cout<< "Actualy balance is: "<< card.mBalance << std::endl;
 }
 
 int main()
@@ -49,8 +70,17 @@ int main()
     Customer john;
 
 	// TODO: Erkläre Peter zu Premium-Kunde, John zu Standard-Kunde
+    peter.mState = CustomerState::Premium;
+    peter.mCreditcard.mState = CreditCardState::Valid;
+
+    john.mState = CustomerState::Standard;
+    john.mCreditcard.mState = CreditCardState::Valid;
 
 	// TODO: Zeige hier an, wieviel Geld Sie zur Verfügung haben
+    std::cout << "Peter: ";
+    showBalance(peter.mCreditcard);
+    std::cout << "John: ";
+    showBalance(john.mCreditcard);
 
     std::cout << "Each is going to withdraw some Money: First 50, then 100 and finally 200."  << std::endl;
     
@@ -64,6 +94,10 @@ int main()
     withdrawMoney(peter, 200);
 
 	// TODO: Zeige hier an, wieviel Geld beide jetzt zur Verfügung haben
+    std::cout << "Peter: ";
+    showBalance(peter.mCreditcard);
+    std::cout << "John: ";
+    showBalance(john.mCreditcard);
 
     return 0;
 }
